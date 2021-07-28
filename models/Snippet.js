a52b8764f9a626897cf848010d3066f1ce9c3e93
maxd66 <max.dunn63@gmail.com>
2021-07-28 11:11:20 -0700

    finished snippet model

--- models/Snippet.js
+++ models/Snippet.js
@@ -10,7 +10,27 @@ Snippet.init(
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
-          }
+        },
+        snippet_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        snippet: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        language: {
+            type: DataTypes.ENUM('HTML', 'CSS', 'Javascript', 'MySql', 'Sequelize', 'Node', 'Express', 'Handlebars', 'Markdown'),
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
     },
     {
     sequelize,
