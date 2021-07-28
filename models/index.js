65174f9d327aca14ce24b097a9b3c9fa3e72c346
maxd66 <max.dunn63@gmail.com>
2021-07-28 13:42:47 -0700

    removed unique false

--- models/index.js
+++ models/index.js
@@ -1,39 +1,37 @@
-const User = require('./User');
-const Tag = require('./Tag');
-const Snippet = require('./Snippet');
-const SnippetTag = require('./SnippetTag');
+const User = require("./User");
+const Tag = require("./Tag");
+const Snippet = require("./Snippet");
+const SnippetTag = require("./SnippetTag");
 
 // Define sequelize associations in this file.
 User.hasMany(Snippet, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
 Snippet.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 User.hasMany(Tag, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Tag.belongsTo(User, {
-    foreignKey: 'user_id',
-})
+  foreignKey: "user_id",
+});
 
 Tag.belongsToMany(Snippet, {
-    through: {
-        model: SnippetTag,
-        unique: false
-    }
+  through: {
+    model: SnippetTag,
+  },
 });
 
 Snippet.belongsToMany(Tag, {
-    through: {
-        model: SnippetTag,
-        unique: false
-    }
-})
+  through: {
+    model: SnippetTag,
+  },
+});
 
 module.exports = { User, Tag, SnippetTag, Snippet };
