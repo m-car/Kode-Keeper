2f1e94a917307cf8a3dfdcaa4b7d34965e811515
maxd66 <max.dunn63@gmail.com>
2021-07-28 11:33:19 -0700

    finished snippetTag model, and user snippet relationship in index.js

--- models/SnippetTag.js
+++ models/SnippetTag.js
@@ -4,7 +4,29 @@ const sequelize = require('../config/connection');
 class SnippetTag extends Model {}
 
 SnippetTag.init({
-
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'tag',
+            key: 'id',
+            unique: false
+        }
+    },
+    snippet_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'snippet',
+            key: 'id',
+            unique: false
+        }
+    }
+    
 },
 {
     sequelize,
