6a5677636a641d823df0f677c57b03090edfc74a
Sam Lloyd <bobdoe784@gmail.com>
2021-07-28 11:30:30 -0700

    Swapped model data between files and added id with references to user

--- models/SnippetTag.js
+++ models/SnippetTag.js
@@ -4,16 +4,7 @@ const sequelize = require('../config/connection');
 class SnippetTag extends Model {}
 
 SnippetTag.init({
-    id:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    tag_name: {
-        type: DataTypes.STRING,
 
-    },
 
 },
 {
