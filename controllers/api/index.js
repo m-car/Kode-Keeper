e89b2142aa39069d0e6d4755c0f631e66af0447d
Marko Caric <pbmc117@gmail.com>
2021-07-30 13:21:06 -0700

    create page now successfuly posts snippet form data and can be found from the find page.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,7 @@
-const router = require('express').Router();
-const usersRouter = require('./users-router');
-router.use('/users', usersRouter);
+const router = require("express").Router();
+const usersRouter = require("./users-router");
+const snippetRouter = require("./snippet-router");
+
+router.use("/users", usersRouter);
+router.use("/snippets", snippetRouter);
 module.exports = router;
