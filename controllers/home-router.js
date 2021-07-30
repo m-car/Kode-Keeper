1edb4538c87f204e97dfdd0c8a6a83972d6c2336
Sam Lloyd <bobdoe784@gmail.com>
2021-07-29 20:48:10 -0700

    Ready for pr

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Tag, Snippet, SnippetTag } = require("../models");
 const withAuth = require("../util/withAuth");
 
 router.get("/", async (req, res) => {
@@ -26,8 +26,42 @@ router.get("/login", (req, res) => {
   res.render("login", { title: "Log-In/Sign-up Page" });
 });
 
-router.get("/find", withAuth, (req, res) => {
-  res.render("find", { title: "Find Snippet" });
+router.get("/find", withAuth, async (req, res) => {
+  // object should look like
+  // {
+  //   snippet-name: 'snippetname',
+  //   snippet: `some
+  //   code here
+  //   and here`,
+  //   language: 'javascript',
+  //   tags: [
+  //     {},
+  //     {}
+  //   ]
+  // }
+  try {
+    const snippetData = await Snippet.findAll({
+      where: {
+        user_id: req.session.userId,
+      },
+      include: [{ model: Tag, through: SnippetTag, as: "tags" }],
+    });
+    if (!snippetData) {
+      res.status(404).json({
+        message:
+          "Oh no, it doesn't look like any snippets match this tag or name",
+      });
+    } else {
+      // const simpleData = snippetData.get({ plain: true });
+      const simpleData = snippetData.map((obj) => {
+        return obj.get({ plain: true });
+      });
+      res.render("find", { simpleData });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 router.get("/create", withAuth, (req, res) => {
